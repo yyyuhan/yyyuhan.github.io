1b786fee3d391b9a456cf21d6a1fbbb5be53a5ae
helenacao <caoyuhan1994@gmail.com>
2021-12-31 23:57:17 +0000

    update modules

---
+++ precache-manifest.4e16802e6c45b8b385aa665fb4141be8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316d463fbe71d88a97d1367757bf2488",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a00b7b8c9eedd134e6de",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b367b5c09802b26db639",
+    "url": "/static/css/main.0652ecd1.chunk.css"
+  },
+  {
+    "revision": "a00b7b8c9eedd134e6de",
+    "url": "/static/js/2.de8b07c9.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.de8b07c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b367b5c09802b26db639",
+    "url": "/static/js/main.c4e57bc3.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
